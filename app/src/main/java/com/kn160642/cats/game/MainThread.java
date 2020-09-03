40f2ca99d4f039c2bd4c322c6379dc6fc3b47e66
Nikola Kesic <nikola.kesic97@gmail.com>
2020-09-03 18:47:52 +0200

    Lock FPS to 60

--- app/src/main/java/com/kn160642/cats/game/MainThread.java
+++ app/src/main/java/com/kn160642/cats/game/MainThread.java
@@ -9,6 +9,9 @@ public class MainThread extends Thread {
     private boolean running;
     public static Canvas canvas;
 
+    private int targetFPS = 60;
+    private double averageFPS;
+
     public MainThread(SurfaceHolder surfaceHolder, GameView gameView) {
         super();
         this.surfaceHolder = surfaceHolder;
@@ -21,7 +24,15 @@ public class MainThread extends Thread {
 
     @Override
     public void run() {
+        long startTime;
+        long timeMillis;
+        long waitTime;
+        long totalTime = 0;
+        int frameCount = 0;
+        long targetTime = 1000 / targetFPS;
+
         while (running) {
+            startTime = System.nanoTime();
             canvas = null;
 
             try {
@@ -30,14 +41,32 @@ public class MainThread extends Thread {
                     this.gameView.update();
                     this.gameView.draw(canvas);
                 }
-            } catch (Exception e) {} finally {
-                if (canvas != null) {
+            } catch (Exception e) {       }
+            finally {
+                if (canvas != null)            {
                     try {
                         surfaceHolder.unlockCanvasAndPost(canvas);
-                    } catch (Exception e) {
+                    }
+                    catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
+            }
+
+            timeMillis = (System.nanoTime() - startTime) / 1000000;
+            waitTime = targetTime - timeMillis;
+
+            try {
+                this.sleep(waitTime);
+            } catch (Exception e) {}
+
+            totalTime += System.nanoTime() - startTime;
+            frameCount++;
+            if (frameCount == targetFPS)        {
+                averageFPS = 1000 / ((totalTime / frameCount) / 1000000);
+                frameCount = 0;
+                totalTime = 0;
+                System.out.println(averageFPS);
             }
         }
     }
